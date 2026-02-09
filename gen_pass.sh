49be3e33ad3c2e14854fa84731a92dce48493e3f
savelbl4 <savelbl4@gmail.com>
2026-02-09 14:28:47 +0300

    passwd generation was modified

--- gen_pass.sh
+++ gen_pass.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-generate_password() {
+generate_strong_password() {
     local length=$1
     < /dev/urandom LC_ALL=C tr -dc 'A-Za-z0-9!@#$%^&*()_+-=[]{}|;:,.<>?/~' | head -c"$length"
+}
+
+generate_simple_password() {
+    local length=$1
+    < /dev/urandom LC_ALL=C tr -dc 'A-Za-z0-9._-' | head -c"$length"
 }
 
 # Функция для экранирования слэшей
@@ -14,14 +19,14 @@ FILE="./inventory/group_vars/netbox/secrets.yml"
 
 [ -a $FILE ] || cp "./inventory/group_vars/netbox/example_secrets.yml" "./inventory/group_vars/netbox/secrets.yml"
 
-VRRP_PASS=$(escape_slashes "$(generate_password 8)")
-PG_SUPER_PASS=$(escape_slashes "$(generate_password 16)")
-REPL_PASS=$(escape_slashes "$(generate_password 15)")
-REDIS_PASS=$(escape_slashes "$(generate_password 12)")
-PGB_ADMIN_PASS=$(escape_slashes "$(generate_password 10)")
-PGB_STATS_PASS=$(escape_slashes "$(generate_password 13)")
+VRRP_PASS=$(escape_slashes "$(generate_strong_password 8)")
+PG_SUPER_PASS=$(escape_slashes "$(generate_strong_password 16)")
+REPL_PASS=$(escape_slashes "$(generate_strong_password 15)")
+REDIS_PASS=$(escape_slashes "$(generate_simple_password 32)")
+PGB_ADMIN_PASS=$(escape_slashes "$(generate_strong_password 10)")
+PGB_STATS_PASS=$(escape_slashes "$(generate_strong_password 13)")
 NETBOX_PEPPERS=$(openssl rand -hex 32)
-NETBOX_SECRET=$(escape_slashes "$(generate_password 50)")
+NETBOX_SECRET=$(escape_slashes "$(generate_strong_password 50)")
 
 sed -i '' "/vrrp_auth_pass: /s/\".*\"/\"$VRRP_PASS\"/" "$FILE"
 sed -i '' "/pg_superuser_password: /s/\".*\"/\"$PG_SUPER_PASS\"/" "$FILE"
